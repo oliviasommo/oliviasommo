0e7167fb3b1f65fce09d32eb793e370f9d402af6
oliviasommo <oliviasommo@gmail.com>
2025-04-13 22:04:18 -0400

    scroll

---
+++ app.js
@@ -0,0 +1,9 @@
+function SplitScroll(){
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene ({
+        duration: 500, 
+        triggerElement: ''
+    })
+}
+SplitScroll():
